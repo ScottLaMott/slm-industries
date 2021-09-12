3ec0d6720ec9c68f47d873408450fbd07abfd8fb
Scott LaMott <slm@becks.ccb>
2021-09-12 10:35:33 +0200

    verbessert

--- ansible/bin/init-enterprise.zsh
+++ ansible/bin/init-enterprise.zsh
@@ -2,9 +2,8 @@
 #
 # bastelt ein vagrant-vm zusammen
 #
-#
 
-# grundsystem
+# grundsystem (user vagrant included)
 ansible-playbook playbooks/fvs.yaml          -i hosts -l enterprise -e 'hostname=enterprise'
 
 # root konfigurieren
@@ -12,8 +11,9 @@ ansible-playbook playbooks/config-root.yaml  -i hosts -l enterprise
 
 # benutzer konfigurieren
 ansible-playbook playbooks/add-user.yaml     -i hosts -l enterprise -e 'username=christop' -e 'fullname=Christoph Timmer' -e 'home=/home/christoph'
-ansible-playbook playbooks/add-user.yaml     -i hosts -l enterprise -e 'username=kirk' -e 'fullname=James D. Kirk' -e 'home=/home/kirk'
-ansible-playbook playbooks/add-user.yaml     -i hosts -l enterprise -e 'username=scotty' -e 'fullname=Montgomery Scott' -e 'home=/home/scotty'
+ansible-playbook playbooks/add-user.yaml     -i hosts -l enterprise -e 'username=kirk'     -e 'fullname=James D. Kirk'    -e 'home=/home/kirk'
+ansible-playbook playbooks/add-user.yaml     -i hosts -l enterprise -e 'username=scotty'   -e 'fullname=Montgomery Scott' -e 'home=/home/scotty'
+ansible-playbook playbooks/add-user.yaml     -i hosts -l enterprise -e 'username=sulu'     -e 'fullname=Sulu'             -e 'home=/home/sulu'
 
 # fzf installieren konfigurieren
 ansible-playbook playbooks/fzf-fuzzy.yaml    -i hosts -l enterprise
