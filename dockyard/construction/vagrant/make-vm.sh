34f27ff34d3479db80260ba69ca17278e5affea7
Scott LaMott <slm@becks.ccb>
2021-09-14 21:46:06 +0200

    .vagrant verzeichnis ignorieren, first import

---
+++ dockyard/construction/vagrant/make-vm.sh
@@ -0,0 +1,5 @@
+: 1631646951:0;vagrant up
+: 1631647496:0;vagrant ssh -c 'sudo apt update'
+: 1631647607:0;vagrant ssh -c 'sudo apt -y upgrade'
+: 1631647607:0;vagrant ssh -c 'sudo apt -y autoremove'
+
