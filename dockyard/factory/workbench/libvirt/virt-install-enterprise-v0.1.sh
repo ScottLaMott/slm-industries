0d759cb69466a87d9c3491babe17758fc0492146
Scott LaMott <slm@becks.ccb>
2022-04-28 19:55:49 +0200

    schön gemacht

--- dockyard/factory/workbench/libvirt/virt-install-enterprise-v0.1.sh
+++ dockyard/factory/workbench/libvirt/virt-install-enterprise-v0.1.sh
@@ -1,9 +1,9 @@
 virt-install  --name        enterprise-v0.1 \
-              --os-type     Linux \
-              --os-variant  archlinux \
-              --memory      8192 \
-              --vcpu        4 \
-              --graphics    spice \
-              --cdrom       /mnt/new-partition/iso-image/artix-base-openrc-20220123-x86_64.iso \
-              --disk        path=/mnt/new-partition/images/enterprise.qcow2,format=qcow2,bus=virtio,size=20 \
-              --check       all=off
+  --os-type     Linux \
+  --os-variant  archlinux \
+  --memory      8192 \
+  --vcpu        4 \
+  --graphics    spice \
+  --cdrom       /mnt/new-partition/iso-image/artix-base-openrc-20220123-x86_64.iso \
+  --disk        path=/mnt/new-partition/images/enterprise.qcow2,format=qcow2,bus=virtio,size=20 \
+  --check       all=off
