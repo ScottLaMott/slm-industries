c29fc51bdc38e9a9464393200e1536d9f413fee8
Scott LaMott <slm@becks.ccb>
2022-05-15 02:57:54 +0200

    first import

---
+++ dockyard/factory/workbench/bin/cat-figlets.sh
@@ -0,0 +1,15 @@
+figlet -t "  enterprise !!!"
+echo
+echo "                   powered by slm-industries.com"
+echo
+echo
+figlet -t "  e n t e r p r i s e   !  !  !"
+echo
+echo "                   powered by slm-industries.com"
+echo
+echo
+figlet -t "  e n t e r p r i s e   v - 0 . 1"
+echo
+echo "                   powered by slm-industries.com"
+echo
+echo
