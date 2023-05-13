37890046f87377c02fd319fe4c3e6fad24325e7c
Scott LaMott <slm@slmi-vm>
2023-05-13 22:53:41 +0200

    btf

--- system/nixos/modules/slmi-environment.nix
+++ system/nixos/modules/slmi-environment.nix
@@ -7,43 +7,20 @@
   #--- list installed packages system profile. To search, run: $ nix search wget
   environment.systemPackages = with pkgs; [
     alacritty
-    bat
-    brave
-    btop
+    bat brave btop
     curl
     dmenu
-    fd
-    file
-    fzf
+    feh fd file fzf
     git
     htop
     ipcalc
     killall
-    lsd
-    lua
-    lua52Packages.luarocks-nix
-    nmap
-    neofetch
-    neovim
-    nodejs
-    pulsemixer
-    pure-prompt
-    python
-    ranger
-    ripgrep
-    rofi
-    screen
-    silver-searcher
-    speedtest-cli
-    stow
-    sxiv
-    tshark
-    tcpdump
-    tig
-    tio
-    toilet
-    tree
-    tldr
+    lsd lua lua52Packages.luarocks-nix
+    nmap neofetch neovim nodejs
+    pulsemixer pure-prompt python
+    ranger ripgrep rofi
+    screen silver-searcher speedtest-cli stow sxiv
+    tshark tcpdump tig tio toilet tree tldr
     wget
     xclip
     vim
