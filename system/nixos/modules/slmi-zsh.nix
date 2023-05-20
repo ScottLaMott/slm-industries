1d7213139e6bf2110e398ca989d7321c9a863394
Scott LaMott <slm@slmi-vm>
2023-05-20 09:45:30 +0200

    base zsh configuration
    - autopair funktioniert nicht
    - autocompletion journalctl/systemctl funktioniert nicht

--- system/nixos/modules/slmi-zsh.nix
+++ system/nixos/modules/slmi-zsh.nix
@@ -8,23 +8,20 @@
   programs.zsh = {
     enable = true;
     defaultKeymap = "viins";
-    enableCompletion = true;     # ??? FIXME
+    enableCompletion = true;
     enableAutosuggestions = true;
     enableSyntaxHighlighting = true;
-    dotDir = ".config/zsh";
-    history = {
-      save = 100000;
-      size = 100000;
-      path = "$XDG_DATA_HOME/zsh/history";
-    };
+    dotDir = ".config/zsh"; # FIXME
+
     zplug = {
       enable = false;
       #zplugHome = ./zsh/zplug;
       #zplugHome = ./zplug-xxx;
       plugins = [
-        { name = "hlissner/zsh-autopair"; tags = [ defer:2 ]; }
+        # { name = "hlissner/zsh-autopair"; tags = [ defer:2 ]; }
       ];
     };
+
     initExtra = ''
       #----------------------------------------------------------
       # declared by configuration.nix / slm
@@ -32,7 +29,7 @@
 
       # own new slm-zshrc
       [ -f  $XDG_CONFIG_HOME/zsh/slm-zshrc ]                && source $XDG_CONFIG_HOME/zsh/slm-zshrc
-      [ -f  $XDG_DATA_HOME/zsh/bin/slm-colored-man-pages ]  && source $XDG_DATA_HOME/zsh/bin/slm-colored-man-pages
+      #[ -f  $XDG_DATA_HOME/zsh/bin/slm-colored-man-pages ]  && source $XDG_DATA_HOME/zsh/bin/slm-colored-man-pages
 
       export LESS='-j5 -R'
 
@@ -40,6 +37,13 @@
 
       #----------------------------------------------------------
     '';
+
+    history = {
+      save = 100000;
+      size = 100000;
+      path = "$XDG_DATA_HOME/zsh/history";
+    };
+
     shellAliases = {
       ".."  =    "cd ..";
       c     =    "clear";
