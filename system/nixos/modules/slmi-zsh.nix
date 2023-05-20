a1d1e153326216fee2fe286efd24bb847ba149f8
Scott LaMott <slm@slmi-vm>
2023-05-20 08:08:54 +0200

    zsh-test

--- system/nixos/modules/slmi-zsh.nix
+++ system/nixos/modules/slmi-zsh.nix
@@ -8,10 +8,38 @@
   programs.zsh = {
     enable = true;
     defaultKeymap = "viins";
-    enableCompletion = true;
+    enableCompletion = true;     # ??? FIXME
     enableAutosuggestions = true;
     enableSyntaxHighlighting = true;
     dotDir = ".config/zsh";
+    history = {
+      save = 100000;
+      size = 100000;
+      path = "$XDG_DATA_HOME/zsh/history";
+    };
+    zplug = {
+      enable = false;
+      #zplugHome = ./zsh/zplug;
+      #zplugHome = ./zplug-xxx;
+      plugins = [
+        { name = "hlissner/zsh-autopair"; tags = [ defer:2 ]; }
+      ];
+    };
+    initExtra = ''
+      #----------------------------------------------------------
+      # declared by configuration.nix / slm
+      #
+
+      # own new slm-zshrc
+      [ -f  $XDG_CONFIG_HOME/zsh/slm-zshrc ]                && source $XDG_CONFIG_HOME/zsh/slm-zshrc
+      [ -f  $XDG_DATA_HOME/zsh/bin/slm-colored-man-pages ]  && source $XDG_DATA_HOME/zsh/bin/slm-colored-man-pages
+
+      export LESS='-j5 -R'
+
+      # wallpaper mit feh
+
+      #----------------------------------------------------------
+    '';
     shellAliases = {
       ".."  =    "cd ..";
       c     =    "clear";
@@ -41,52 +69,6 @@
       cmr   =    "cmatrix -u9 -r";
       cct   =    "/usr/bin/theme.sh -i2";
     };
-    history = {
-      save = 100000;
-      size = 100000;
-      path = "$XDG_DATA_HOME/zsh/history";
-    };
-    zplug = {
-      enable = true;
-      #zplugHome = ./zsh/zplug;
-      #zplugHome = ./zplug-xxx;
-
-      # FIXME FIXME FIXME ---------------------------------------
-      # alle ungültig weil SHELL-Environment
-      #zplugHome = ${XDG_CONFIG_HOME}/zsh/zplug;
-      #zplugHome = $XDG_CONFIG_HOME/zsh/zplug;
-      #zplugHome = "${XDG_CONFIG_HOME}/zsh/zplug";
-      #zplugHome = "$XDG_DATA_HOME/zsh/zplug";
-      #zplugHome = $XDG_DATA_HOME/zsh;
-      #zplugHome = $XDG_CONFIG_HOME/zsh;
-      #zplugHome = "$XDG_CONFIG_HOME/zsh/zplug";
-
-      #zplugHome = "${config.xdg.dataHome}/zsh/zplug";
-      #zplugHome = "${config.xdg.configHome}/zsh/zplug";
-      #zplugHome = $config.xdg.dataHome/zsh/zplug;
-      #zplugHome = ${config.xdg.dataHome}/zsh/zplug;
-      # FIXME FIXME FIXME ---------------------------------------
-
-      plugins = [
-        { name = "hlissner/zsh-autopair"; tags = [ defer:2 ]; }
-        # { name = "zsh-users/zsh-autosuggestions"; tags = [ defer:2 ]; ?? on:"zsh-users/zsh-completions" ?? }
-      ];
-    };
-    initExtra = ''
-      #----------------------------------------------------------
-      # declared by configuration.nix / slm
-      #
-
-      # own new slm-zshrc
-      [ -f  $XDG_CONFIG_HOME/zsh/slm-zshrc ]                && source $XDG_CONFIG_HOME/zsh/slm-zshrc
-      [ -f  $XDG_DATA_HOME/zsh/bin/slm-colored-man-pages ]  && source $XDG_DATA_HOME/zsh/bin/slm-colored-man-pages
-
-      export LESS='-j5 -R'
-
-      # wallpaper mit feh
-
-      #----------------------------------------------------------
-    '';
   };
 
 }
