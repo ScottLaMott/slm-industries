6feea56e7ff0b510fa378538648aa702e821399a
Scott LaMott <slm@slmi-vm>
2023-08-12 22:53:43 +0200

    new package mutt

--- system/nixos/modules/slmi-environment.nix
+++ system/nixos/modules/slmi-environment.nix
@@ -20,7 +20,7 @@
     iftop inxi ipcalc iptraf-ng
     killall
     lsd lua lua52Packages.luarocks-nix
-    mixxx
+    mixxx mutt
     nmap neofetch neovim nodejs
     pciutils pulsemixer pure-prompt python
     ranger ripgrep rofi
