763c274acbdbc6f8a8f42a440974099c813da911
Scott LaMott <slm@slmi-vm>
2023-08-13 02:35:58 +0200

    vim navigation

--- system/nixos/modules/slmi-tmux.nix
+++ system/nixos/modules/slmi-tmux.nix
@@ -18,6 +18,11 @@
       set-option -g mouse on
       set-option -g renumber-windows on
       TMUX_FZF_LAUNCH_KEY="C-f"        # tmux-fzf plugin c-a c-f
+      
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
     '';
     plugins = [
       pkgs.tmuxPlugins.gruvbox
