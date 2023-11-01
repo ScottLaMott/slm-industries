a56edfca5503fe25a77a895b575d6c3e44064e70
Scott LaMott <slm@slmi-industries>
2023-11-01 19:59:44 +0100

    rofi test

--- system/nixos/modules/slmi-rofi.nix
+++ system/nixos/modules/slmi-rofi.nix
@@ -7,24 +7,26 @@
 
   programs.rofi = {
     enable=true;
-    theme="sidebar";
+
+    theme="/home/slm/.config/rofi/launchers/type-slm/style-slm.rasi";
+    # theme="/home/slm/.config/rofi/launchers/type-7/style-9.rasi";
+
     terminal="alacritty";
-    # extraConfig  = {
-    #   location = 1;
-    #   xoffset  = 20;
-    #   yoffset  = 0;
-    #   display-window      = "window > ";
-    #   display-windowcd    = "windowcd > ";
-    #   display-run         = "run > ";
-    #   display-ssh         = "ssh > ";
-    #   display-drun        = "drun > ";
-    #   display-combi       = "combi > " ;
-    #   display-keys        = "keys > " ;
-    #   display-filebrowser = "filebrowser > ";
-    # };
-    configPath="$XDG_CONFIG_HOME/rofi/config.rasi";
+    extraConfig  = {
+      location = 1;
+      xoffset  = 20;
+      yoffset  = 0;
+      display-window      = "window > ";
+      display-windowcd    = "windowcd > ";
+      display-run         = "run > ";
+      display-ssh         = "ssh > ";
+      display-drun        = "drun > ";
+      display-combi       = "combi > " ;
+      display-keys        = "keys > " ;
+      display-filebrowser = "filebrowser > ";
+    };
+    configPath="/home/slm/.config/rofi/config.rasi";
   };
 
 }
-
 
