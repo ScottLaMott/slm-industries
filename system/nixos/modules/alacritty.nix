8d26d248da4f2a24d143c4a6a2e3f99ceee8a8ec
Scott LaMott <slm@slmi-industries>
2023-11-25 14:17:14 +0100

    btf

--- system/nixos/modules/alacritty.nix
+++ system/nixos/modules/alacritty.nix
@@ -10,8 +10,15 @@
     #--- settings
     settings = {
       font.size = 9;
+      font.x = 0;
+      font.y = 2;
       window.opacity = 0.9;
       window.padding.x = 8;
+      window.padding.y = 8;
+      cursor.style.shape = "Underline";
+      cursor.style.blinking = "Always";
+      cursor.blink_interval = 150;
+      cursor.blink_timeout = 3;
     };
   };
 }
