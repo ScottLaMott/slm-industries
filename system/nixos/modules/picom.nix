76a4576d6f5a360cc0c648d85705f79e3f637d2f
Scott LaMott <slm@slmi-industries>
2023-11-29 19:40:23 +0100

    window manager schön gemacht

--- system/nixos/modules/picom.nix
+++ system/nixos/modules/picom.nix
@@ -6,9 +6,22 @@
 { config, lib, pkgs, modulesPath, ... }: {
 
   services.picom = {
-    enable = true;
-    fade = true;
-    shadow = true;
+    enable  = true;
+    backend = "glx";
+    fade    = true;
+    shadow  = true;
+    vSync   = true;
+
+    settings.blur = {
+        method = "dual_kawase";
+        size = 3;
+        deviation = 5.0;
+    };
+    settings.fade-delta    = 10;
+    settings.corner-radius = 10;
+    settings.rounded-corners-exclude = [
+        "_NET_WM_WINDOW_TYPE@[0]:a = '_NET_WM_WINDOW_TYPE_DOCK'"
+    ];
   };
 
 }
