e42613d409ba652229a1bbff39620044eb89f3e5
Scott LaMott <slm@slmi-industries>
2023-12-22 19:14:19 +0100

    alacritty beautified, picom

--- system/nixos/modules/alacritty.nix
+++ system/nixos/modules/alacritty.nix
@@ -13,8 +13,10 @@
       font.x = 0;
       font.y = 2;
       window.opacity = 0.8;
-      window.padding.x = 8;
-      window.padding.y = 3;
+      window.padding.x = 0;
+      window.padding.y = 0;
+      # window.padding.x = 8;
+      # window.padding.y = 3;
       cursor.style.shape = "Underline";
       cursor.style.blinking = "Always";
       cursor.blink_interval = 150;
