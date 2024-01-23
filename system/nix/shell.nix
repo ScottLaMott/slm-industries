e004d3bf9ee60b06fde7fa132bfa8bd0e1b4c010
Scott LaMott <slm@slmi-industries>
2024-01-23 20:06:40 +0100

    nix package testverzeichnis

---
+++ system/nix/shell.nix
@@ -0,0 +1,25 @@
+
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+  pkgs.mkShell {
+    packages = with pkgs; [
+      cmatrix
+      figlet
+      sl
+      vim
+    ];
+
+    GIT_EDITOR = "${pkgs.neovim}/bin/nvim";
+
+    shellHook = ''
+      clear
+      echo
+      echo
+      toilet -f future 'slm-industries . . .'
+      echo
+      git status
+    '';
+  }
