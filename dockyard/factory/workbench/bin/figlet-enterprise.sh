f87bbb4e8b688a8cb73dccc631743ae00badea2a
Scott LaMott <slm@slmi-industries>
2024-02-21 19:18:42 +0100

    neue figlets

---
+++ dockyard/factory/workbench/bin/figlet-enterprise.sh
@@ -0,0 +1,14 @@
+#!/run/current-system/sw/bin/bash
+
+echo
+figlet -t "  e n t e r p r i s e   v - 0 . 1"
+echo
+echo "                   powered by slm-industries.com"
+echo
+
+echo
+figlet -t "  e n t e r p r i s e   - 0 0 1"
+echo
+echo "                   powered by slm-industries.com"
+echo
+
