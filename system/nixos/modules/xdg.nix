4c5b68e19f93b07632a0c434abe8359fe14eab55
Scott LaMott <slm@slm-industries>
2024-06-13 02:37:50 +0200

    xdg verbessert, siehe blog cola gang

--- system/nixos/modules/xdg.nix
+++ system/nixos/modules/xdg.nix
@@ -7,6 +7,10 @@
 
   xdg = {
     enable = true;
+
+    configFile."awesome/rc.lua".source   = ../dots/awesome/rc.lua;
+    configFile."vim/settings.vim".source = ../dots/vim/settings.vim;
+    configFile."vim/maps.vim".source     = ../dots/vim/maps.vim;
   };
 
 }
