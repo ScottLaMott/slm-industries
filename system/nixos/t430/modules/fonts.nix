4a3d704fae043026502e5feb0e420b6e872ccefa
Scott LaMott <slm@slm-industries>
2025-06-24 18:12:51 +0200

    fonts test

--- system/nixos/t430/modules/fonts.nix
+++ system/nixos/t430/modules/fonts.nix
@@ -3,6 +3,11 @@
 { pkgs, ... }: {
 
   fonts.packages = with pkgs; [
+    # nerd-fonts._0xproto
     nerd-fonts.fira-code
+    # nerd-fonts.mononoki
+    # nerd-fonts.lekton
+    # nerd-fonts.victor-mono
+    # nerd-fonts.profont
   ];
 }
