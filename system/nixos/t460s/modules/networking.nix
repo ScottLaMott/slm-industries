db17413bd29c3570f11783826966a094529f12fa
Scott LaMott <slm@slm-industries>
2025-06-25 13:46:14 +0200

    networking improved

--- system/nixos/t460s/modules/networking.nix
+++ system/nixos/t460s/modules/networking.nix
@@ -1,26 +1,27 @@
+#----------------------------------------------------------
+#---
+#--- networking configuration
+#---
+
 { config, pkgs, ... }: {
-  networking.networkmanager.enable = true;
-  networking.hostName = "t460s-github";
-  # networking.hostName = "slm-industries";
-  networking.extraHosts =
-    ''
-      # ubuntu-container
-      10.151.127.10   ubuntu-23-04
-      10.151.127.101  u-1
-      10.151.127.102  u-2
-      10.151.127.109  u-23-10
-      10.151.127.70   u-23-10-nng
-      10.151.127.111  u-ansible
-      10.151.127.112  u-tmux
-      10.151.127.113  u-zplug
+
+  networking = {
+   
+    networkmanager.enable = true;
+    hostName = "t460";
+    nftables.enable = true;
+    
+    firewall.interfaces.incusbr0.allowedTCPPorts = [
+      53
+      67
+    ];
+    firewall.interfaces.incusbr0.allowedUDPPorts = [
+      53
+      67
+    ];
+   
+    extraHosts = ''
       # enterprise-container
-      10.151.127.200  enterprise-v-0-1
-      10.151.127.201  et-1
-      10.151.127.202  et-2
-      # alpine containers
-      10.151.127.120  alpine
-      10.151.127.130  alpine-test
-      10.151.127.131  a-1
-      10.151.127.132  a-2
     '';
+  };
 }
