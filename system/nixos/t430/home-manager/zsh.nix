df776e7a004d386d60dd4097b3a26d77ede0b1f7
Scott LaMott <slm@slm-industries>
2025-06-27 18:38:34 +0200

    zsh config modified
    zsh configuration teilweise von dots/zsh nach home-manager verschoben

--- system/nixos/t430/home-manager/zsh.nix
+++ system/nixos/t430/home-manager/zsh.nix
@@ -12,8 +12,7 @@
   programs.zsh = {
     enable = true;
     defaultKeymap = "viins";
-    # enableCompletion = false; # FIXME, true macht zsh-startup langsam
-    enableCompletion = true; # FIXME, VIELLEICHT gefixt in 25.05, testen
+    enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     dotDir = ".config/zsh"; # FIXME
@@ -21,7 +20,7 @@
     zplug = {
       enable = true;
       plugins = [
-        # { name = "hlissner/zsh-autopair"; tags = [ "defer:2" ]; }
+        { name = "hlissner/zsh-autopair"; tags = [ "defer:2" ]; }
         { name = "endaaman/lxd-completion-zsh"; }
         { name = "agkozak/zsh-z"; }
       ];
@@ -42,12 +41,6 @@
       EDITOR=nvim
     '';
 
-    #initExtraFirst = ''
-    #  #
-    #  # profile zsh-startup
-    #  zmodload zsh/zprof
-    #'';
-
     initContent = ''
       #----------------------------------------------------------
       # initContent declared by .zshrc in zsh.nix
@@ -57,7 +50,16 @@
       [ -f  $XDG_CONFIG_HOME/zsh/slm-zshrc ]                && source $XDG_CONFIG_HOME/zsh/slm-zshrc
       [ -f  $XDG_CONFIG_HOME/zsh/slm-colored-man-pages ]    && source $XDG_CONFIG_HOME/zsh/slm-colored-man-pages
 
-      # export LESS='-j8 -R'
+      #--- completion
+      zmodload zsh/complist
+      #
+      # use vim keys in tab complete menu:
+      zstyle ':completion:*' menu select
+      bindkey -M menuselect 'h' vi-backward-char
+      bindkey -M menuselect 'k' vi-up-line-or-history
+      bindkey -M menuselect 'l' vi-forward-char
+      bindkey -M menuselect 'j' vi-down-line-or-history
+      bindkey -v '^?' backward-delete-char
 
       # wallpaper mit feh
 
