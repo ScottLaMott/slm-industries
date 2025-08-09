d94fe393afbb4a387df9aeeef5c268613e69e858
Scott LaMott <slm@slm-industries>
2025-08-09 16:14:47 +0200

    xdg verbessert

--- system/nixos/t460s/home-manager/xdg.nix
+++ system/nixos/t460s/home-manager/xdg.nix
@@ -8,17 +8,36 @@
   xdg = {
     enable = true;
 
-    configFile."awesome/rc.lua".source   = ../dots/awesome/rc.lua;
-    configFile."vim/settings.vim".source = ../dots/vim/settings.vim;
-    configFile."vim/maps.vim".source     = ../dots/vim/maps.vim;
-    configFile."zsh/slm-zshrc".source    = ../dots/zsh/slm-zshrc;
-    configFile."zsh/slm-colored-man-pages".source = ../dots/zsh/slm-colored-man-pages;
+    configFile."awesome".enable = true;
+    configFile."awesome".source = ../dots/awesome/rc.lua;
+    configFile."awesome".target = "awesome/rc.lua";
+
+    configFile."vim-settings".enable = true;
+    configFile."vim-settings".source = ../dots/vim/settings.vim;
+    configFile."vim-settings".target = "vim/settings.vim";
+
+    configFile."vim-maps".enable = true;
+    configFile."vim-maps".source = ../dots/vim/maps.vim;
+    configFile."vim-maps".target = "vim/maps.vim";
     
-    configFile."yazi/yazi.toml".source   = ../dots/yazi/yazi.toml;
+    configFile."zsh".enable = true;
+    configFile."zsh".source = ../dots/zsh/slm-zshrc;
+    configFile."zsh".target = "zsh/zshrc";
     
+    configFile."zsh-slm-colored-man-pages".source = ../dots/zsh/slm-colored-man-pages;
+    
+    configFile."yazi".enable   = true;
+    configFile."yazi".source   = ../dots/yazi/yazi.toml;
+    configFile."yazi".target   = "../yazi/yazi.toml";
+   
+    #-- not following xdg standart
     configFile."tcl".enable   = true;
     configFile."tcl".source   = ../dots/tcl/tclshrc;
     configFile."tcl".target   = "../.tclshrc";
+  
+    configFile."input".enable   = true;
+    configFile."input".text   = "set editing-mode vi";
+    configFile."input".target   = "../.inputrc";
   
   };
 
