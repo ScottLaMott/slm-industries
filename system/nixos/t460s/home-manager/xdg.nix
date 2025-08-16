3d26557206457c507b467c23501b3935c68fc4d3
Scott LaMott <slm@slm-industries>
2025-08-16 14:42:29 +0200

    btf ...

--- system/nixos/t460s/home-manager/xdg.nix
+++ system/nixos/t460s/home-manager/xdg.nix
@@ -8,32 +8,50 @@
   xdg = {
     enable = true;
 
-    configFile."awesome".enable = true;
-    configFile."awesome".source = ../dots/awesome/rc.lua;
-    configFile."awesome".target = "awesome/rc.lua";
+    configFile."awesome" = {
+      enable = true;
+      source = ../dots/awesome/rc.lua;
+      target = "awesome/rc.lua";
+    };
 
-    configFile."vim-settings".enable = true;
-    configFile."vim-settings".source = ../dots/vim/settings.vim;
-    configFile."vim-settings".target = "vim/settings.vim";
+    configFile."vim-settings" = {
+      enable = true;
+      source = ../dots/vim/settings.vim;
+      target = "vim/settings.vim";
+    };
 
-    configFile."vim-maps".enable = true;
-    configFile."vim-maps".source = ../dots/vim/maps.vim;
-    configFile."vim-maps".target = "vim/maps.vim";
+    configFile."vim-maps" = {
+      enable = true;
+      source = ../dots/vim/maps.vim;
+      target = "vim/maps.vim";
+    };
     
-    configFile."zsh".enable = true;
-    configFile."zsh".source = ../dots/zsh/slm-zshrc;
-    configFile."zsh".target = "zsh/slm-zshrc";
+    configFile."zsh" = {
+      enable = true;
+      source = ../dots/zsh/slm-zshrc;
+      target = "zsh/slm-zshrc";
+    };
     
-    configFile."zsh-slm-colored-man-pages".source = ../dots/zsh/slm-colored-man-pages;
+    configFile."zsh.scmp" = {
+      enable = true;
+      source = ../dots/zsh/slm-colored-man-pages;
+      target = "zsh/slm-colored-man-pages";
+    };
+
+    configFile."yazi" = {
+      enable   = true;
+      source   = ../dots/yazi/yazi.toml;
+      target   = "yazi/yazi.toml";
+    };
     
-    configFile."yazi".enable   = true;
-    configFile."yazi".source   = ../dots/yazi/yazi.toml;
-    configFile."yazi".target   = "../yazi/yazi.toml";
-   
     #-- not following xdg standart
-    configFile."tcl".enable   = true;
-    configFile."tcl".source   = ../dots/tcl/tclshrc;
-    configFile."tcl".target   = "../.tclshrc";
+    
+    configFile."tcl" = {
+      enable   = true;
+      source   = ../dots/tcl/tclshrc;
+      target   = "../.tclshrc";
+    };
+    
   };
 
 }
