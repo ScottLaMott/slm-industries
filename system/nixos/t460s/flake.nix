2811221369817bc384bf140a72947d91ebb3985d
Scott LaMott <slm@slm-industries>
2025-08-23 10:53:39 +0200

    vorbereitung für nixos-next-generation
    - mit home-manager
    - mit modules
    - flacky

---
+++ system/nixos/t460s/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "t460s-next-generation";
+
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.t460s-ng = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
+    };
+  };
+}
