f826d8ed5f9c90deba7a0a865b8105da24daf579
Scott LaMott <slm@slm-industries>
2026-05-10 12:45:51 +0200

    mit hostname experimentiert

--- system/nixos/t460s-ng/modules/nixos/networking.nix
+++ system/nixos/t460s-ng/modules/nixos/networking.nix
@@ -8,7 +8,7 @@
   networking = {
    
     networkmanager.enable = true;
-    hostName = "t460s-co";
+    hostName = "t460s-ng";
     nftables.enable = true;
     
     firewall.interfaces.incusbr0.allowedTCPPorts = [
